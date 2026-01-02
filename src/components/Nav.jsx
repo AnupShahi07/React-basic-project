ac24c6fc7dcde68c284b209baf4874d0e1df9680
Anup shahi <bhanushahi70@gmail.com>
2026-01-02 22:03:34 +0530

    added chnages

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom';
 function Nav() {
     return (
         <div className='Navbar'>
-            <h2 className='name'>Welcome to React App</h2>
+            <h2 className='name'>Welcome to My Project</h2>
             <div className='link'>  
                 <Link to='/'>Home</Link>
                 <Link to='/about'>About</Link>
                 <Link to='/photos'>Photos</Link>
+                <Link to='/contact'>Contact</Link>
             </div>
         </div>
     )
